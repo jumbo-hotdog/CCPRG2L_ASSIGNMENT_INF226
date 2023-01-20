1db929738a15977b3b857027c8d749e1425b23ce
Joshua Guimbaolibot <119920263+jumbo-hotdog@users.noreply.github.com>
2023-01-20 15:59:27 +0800

    Add files via upload

---
+++ Exercise3/Character.java
@@ -0,0 +1,20 @@
+package ClassesAndObjects;
+
+public class Character {
+    int strength;
+    int agility;
+    int intelligence;
+    String name;
+
+    public void sayMyName() {
+        System.out.println("Hello I am " + name);
+    }
+    
+    public void sayMyStrength() {
+        System.out.println("My strength is " + strength);
+    }
+
+    void attack() {
+        System.out.println("I am attacking!");
+    }
+}
---
+++ Exercise3/Weapon.java
@@ -0,0 +1,19 @@
+package ClassesAndObjects;
+
+public class Weapon {
+    String name;
+    int damage;
+    String rarity;
+
+    void add (int addedDamage) {
+        int newDamage = this.damage + addedDamage;
+
+        System.out.println(addedDamage + " damage has been added. Total damage is " + newDamage + ".");
+
+        this.damage = newDamage;
+    }
+
+    String weaponInfo () {
+        return "Weapon name: " + name + "\n" + "Rarity: " + rarity;
+    }
+}
