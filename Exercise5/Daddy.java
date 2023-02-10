fd1290fa0c46472a6043e80b40540ae7e8c737e6
Joshua Guimbaolibot <119920263+jumbo-hotdog@users.noreply.github.com>
2023-02-10 14:20:57 +0800

    Add files via upload

---
+++ Exercise5/Daddy.java
@@ -0,0 +1,11 @@
+package exercise1;
+
+public class Daddy {
+    
+    protected String surname = "Guimbaolibot";
+
+    protected void showSurname() {
+        System.out.println("My family name is: " + surname);
+    }
+
+}
---
+++ Exercise5/Person.java
@@ -0,0 +1,15 @@
+package exercise1;
+
+public class Person {
+    
+    private String name = "Joshua";
+    
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
+
